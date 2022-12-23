49ca5d2406325e8fb5920d25fb13edd301459317
First <ddamage11@gmail.com>
2022-12-23 15:46:16 +0600

    treug

--- с# return (1)/лекции/лекция1/практика/Program.cs
+++ с# return (1)/лекции/лекция1/практика/Program.cs
@@ -41,3 +41,41 @@ else
 {
 Console.WriteLine("привет, " + username);
 } */
+
+// перетаскивание гирь
+
+/* int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+if(a > max) max =a;
+if(b > max) max =b;
+if(c > max) max =c;
+if(d > max) max =d;
+if(e > max) max =e;
+
+Console.Write("max = ");
+Console.WriteLine(max); */
+
+// цикл
+
+Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+Console.SetCursorPosition(xb,yb);
+Console.Write("+");
+Console.SetCursorPosition(xc,yc);
+Console.Write("+");
