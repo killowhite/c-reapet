b90cd1012696e953a8e42c6f056346c80b43a950
First <ddamage11@gmail.com>
2022-12-23 14:47:54 +0600

    random

--- с# return (1)/лекции/лекция1/практика/Program.cs
+++ с# return (1)/лекции/лекция1/практика/Program.cs
@@ -1,5 +1,29 @@
 ﻿// приветствие пользователя
-Console.WriteLine("Введите ваше имя");
+
+/* Console.WriteLine("Введите ваше имя");
 String username = Console.ReadLine();
 Console.Write("Привет, ");
-Console.Write(username);
+Console.Write(username); */
+
+// сложить 2 числа
+
+/* int numberA = 3;
+int numberB = 5;
+//Console.WriteLine(numberA+numberB);
+int result = numberA + numberB;
+Console.WriteLine(result); */
+
+// частное двух чисел
+
+/* double numberA = 10;
+double numberB = 5;
+Console.WriteLine(numberA/numberB) */
+
+// частное двух случайных чисел
+
+double numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
