87e6e4c1b720998f7c28b25133acb8f62e107f4d
First <ddamage11@gmail.com>
2022-12-23 15:07:47 +0600

    privet

--- с# return (1)/лекции/лекция1/практика/Program.cs
+++ с# return (1)/лекции/лекция1/практика/Program.cs
@@ -21,9 +21,23 @@ Console.WriteLine(numberA/numberB) */
 
 // частное двух случайных чисел
 
-double numberA = new Random().Next(1,10);
+/* double numberA = new Random().Next(1,10);
 Console.WriteLine(numberA);
 double numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 double result = numberA / numberB;
-Console.WriteLine(result);
+Console.WriteLine(result); */
+
+/* на ввод имя если Михаил то вывод привет босс.
+если другое, то просто привет*/
+
+/* Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "Михаил")
+{
+    Console.WriteLine(" привет босс");
+}
+else
+{
+Console.WriteLine("привет, " + username);
+} */
