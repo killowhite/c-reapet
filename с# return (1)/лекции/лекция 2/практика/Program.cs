1da20f6b351b6758b63075a6d13265024f0c6fcb
First <ddamage11@gmail.com>
2022-12-26 06:35:06 +0600

    final

---
+++ с# return (1)/лекции/лекция 2/практика/Program.cs
@@ -0,0 +1,25 @@
+﻿// найти максимальное число из 9 чисел
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 17;
+int b1 = 23;
+int c1 = 20;
+int a2 = 27;
+int b2 = 33;
+int c2 = 30;
+int a3 = 37;
+int b3 = 43;
+int c3 = 40;
+
+/* int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);*/
+int max =Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
